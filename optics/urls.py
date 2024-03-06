0766dce6f46d05bdf69b90ab26c15f9f259fe9c8
Jason van Lint <jvanlint@mac.com>
2024-03-06 11:53:34 +0000

    Add static file serving during development

--- optics/urls.py
+++ optics/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,11 @@ urlpatterns = [
     # Account management is done by allauth
     path("accounts/", include("allauth.urls")),
 ]
+
+# Use static() to add url mapping to serve static files during development (only)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
