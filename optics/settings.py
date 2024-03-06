9c115f13f7009ed90ec76fe7cd11c306bb539a32
Jason van Lint <jvanlint@mac.com>
2024-03-06 02:35:56 +0000

    Update INSTALLED_APPS in settings.py

--- optics/settings.py
+++ optics/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "optics.opticsapp",
+    "optics.opticsapp.apps.OpticsappConfig",
 ]
 
 MIDDLEWARE = [
