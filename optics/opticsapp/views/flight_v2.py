bf7dd12b4316c5ed6e2254ded84afcf2257aae9f
Jason van Lint <jvanlint@mac.com>
2024-03-14 13:32:38 +1100

    Add targets to flight object update.

--- optics/opticsapp/views/flight_v2.py
+++ optics/opticsapp/views/flight_v2.py
@@ -127,6 +127,8 @@ def flight_update_v2(request, link_id):
             obj.modified_by = request.user
             # TODO: When airframe type is updated the aircraft should be updated.
             obj.save()
+            obj.targets.set(post_form.cleaned_data['targets'])
+            obj.save()
             return HttpResponseRedirect(returnURL)
 
     context = {
