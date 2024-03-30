94830694d459b56a2731336a5524fdb884002bdb
Steve <github@ausaviators.com>
2024-03-30 15:43:39 +1100

    Adjusted Make_Choices to only list flyable units
    simplified this a lot

--- make_choices.py
+++ make_choices.py
@@ -2,29 +2,18 @@ from dcs.helicopters import helicopter_map
 from dcs.planes import plane_map
 
 
-def convert_dicts():
-    plane_list = list(key for key, value in plane_map.items())
-    helicopter_list = list(key for key, value in helicopter_map.items())
+def get_flyable_aircraft():
+    flyable_planes = [(id, plane) for id, plane in plane_map.items() if plane.flyable]
+    flyable_helos = [(id, helo) for id, helo in helicopter_map.items() if helo.flyable]
 
-    print("dcsname")
-    for idx1, plane in enumerate(sorted(plane_list)):
-        db_value = (
-            plane.replace(" ", "_").replace("/", "").replace("-", "_").replace(".", "_")
-        )
-        # print(f"        {db_value} = '{plane}'")
-        print(f"{db_value}")
-    for idx2, helicopter in enumerate(sorted(helicopter_list)):
-        db_value = (
-            helicopter.replace(" ", "_")
-            .replace("/", "")
-            .replace("-", "_")
-            .replace(".", "_")
-        )
-        print(f"{db_value}")
+    for plane in flyable_planes:
+        print(plane[1].id)
+    for helicopter in flyable_helos:
+        print(helicopter[1].id)
 
 
 if __name__ == "__main__":
-    convert_dicts()
+    get_flyable_aircraft()
     print("-" * 80)
     print('Built airframe type choices from pydcs')
     print("-" * 80)
